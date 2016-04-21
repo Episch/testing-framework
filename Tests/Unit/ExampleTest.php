6765d258252f2c19f8f600fe8fad471b53fcc29c
David <info.ansichtssache@gmail.com>
2016-04-21 22:24:03 +0200

    Initial Commit

---
+++ Tests/Unit/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+//namespace TYPO3\CMS\Backend\Tests\Unit\Utility;
+
+/**
+ * Test case
+ */
+class ExampleTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
+{
+
+    /**
+     * simple example test
+     *
+     * @test
+     */
+    public function exampleTest()
+    {
+        $iconClass = 't3-icon';
+        $this->assertEquals('t3-icon', $iconClass);
+    }
+
+}
---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Testing Framework',
+    'description' => 'Php Unit easy example test',
+    'category' => 'misc',
+    'author' => 'David Nagelschmidt',
+    'author_email' => 'david@episch.org',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
